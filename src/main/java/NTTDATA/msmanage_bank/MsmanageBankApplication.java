a6967bfca7897e0c4b0b878d4feb1cb64c3b21d8
Dtravezano <Davidu.0622@gmail.com>
2021-11-22 16:19:53 -0500

    start project

---
+++ src/main/java/NTTDATA/msmanage_bank/MsmanageBankApplication.java
@@ -0,0 +1,13 @@
+package NTTDATA.msmanage_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmanageBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmanageBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NTTDATA/msmanage_bank/MsmanageBankApplicationTests.java
@@ -0,0 +1,13 @@
+package NTTDATA.msmanage_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmanageBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
